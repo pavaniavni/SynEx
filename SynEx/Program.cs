3a71620c37ef62aa0c5c78f3d53a43c6ac19cb04
pavani <pavaniavni456@gmail.com>
2022-02-10 11:41:08 +0530

    added SynEx

---
+++ SynEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread work1Thread = new Thread(Work1);
+            Thread work2Thread = new Thread(Work2);
+            work1Thread.Start();
+            work2Thread.Start();
+            Console.ReadLine();
+        }
+        static void Work1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work1 is called " + i.ToString());
+            }
+
+        }
+        static void Work2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work2 is called " + i.ToString());
+            }
+        }
+    }
+}
